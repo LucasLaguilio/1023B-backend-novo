644223bb964e7b7fb150822cf3f3593162a6c149
Lucas Laguilio <laguiliolucas@gmail.com>
2025-09-17 11:32:47 -0400

    Adicionando metodo POST

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -16,8 +16,20 @@ app.get('/', async (_:Request, res:Response) => {
 })
 app.get('/produtos', async (_:Request, res:Response) => {
     const produtos = await db.collection('produtos').find().toArray()
-    res.json(produtos)
+     res.status(200).json(produtos)
 })
+
+app.post('/produtos', async (req:Request, res:Response) => {
+   const {nome,preco,urlfoto,descricao} = req.body
+   if (!nome || !preco || !urlfoto || !descricao) {
+    return res.status(400).json({error: 'Nome, preço, urlfoto e descrição são obrigatórios'})
+   }
+   
+   const produto = {nome,preco,urlfoto,descricao}
+   const result = await db.collection('produtos').insertOne(produto)
+    res.status(201).json({nome, preco, urlfoto, descricao, _id:result.insertedId})
+
+}) 
 // Criando o servidor na porta 8000 com o express
 app.listen(8000, () => {
     console.log('Server is running on port 8000')
