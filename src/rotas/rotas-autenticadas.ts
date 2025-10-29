d505f4d7cb971f6a95033c54ba2e832649d244ee
Laurizylinda <laurizy.santos@estudante.ifms.edu.br>
2025-10-29 11:43:00 -0400

    adicionei adimin no back, trudo errado

--- src/rotas/rotas-autenticadas.ts
+++ src/rotas/rotas-autenticadas.ts
@@ -1,15 +1,22 @@
-import {Router} from 'express'
+import { Router } from "express";
+import carrinhoController from "../carrinho/carrinho.controller.js";
+import produtosController from "../produtos/produtos.controller.js";
+import adiminPageController from "../adiminPage/adiminPageController.js";
+import { apenasAdmin } from "../middlewares/apenasAdmin.js";
 
-import carrinhoController from '../carrinho/carrinho.controller.js'
-import produtosController from '../produtos/produtos.controller.js'
+const rotas = Router();
 
-const rotas = Router()
-rotas.post('/produtos',produtosController.adicionar)
-rotas.get('/produtos',produtosController.listar)
 
-rotas.post('/adicionarItem',carrinhoController.adicionarItem)
-rotas.post('/removerItem',carrinhoController.removerItem)
-rotas.get('/carrinho/:usuarioId',carrinhoController.listar)
-rotas.delete('/carrinho/:usuarioId',carrinhoController.remover)
+rotas.post("/produtos", produtosController.adicionar);
+rotas.get("/produtos", produtosController.listar);
 
-export default rotas
+
+rotas.post("/adicionarItem", carrinhoController.adicionarItem);
+rotas.post("/removerItem", carrinhoController.removerItem);
+rotas.get("/carrinho/:usuarioId", carrinhoController.listar);
+rotas.delete("/carrinho/:usuarioId", carrinhoController.remover);
+
+
+rotas.get("/admin/usuarios", apenasAdmin, adiminPageController.listarTodos);
+
+export default rotas;
